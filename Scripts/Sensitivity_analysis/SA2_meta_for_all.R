2dd2528f2c6f00a69eefdc84812adac5273b93cc
rafalopespx <lopes1313@gmail.com>
2023-11-22 12:44:26 +0100

    New run sensitivity analysis

--- Scripts/Sensitivity_analysis/SA2_meta_for_all.R
+++ Scripts/Sensitivity_analysis/SA2_meta_for_all.R
@@ -3,18 +3,19 @@ gc()
 
 ### Loading packages
 source("Scripts/01_parametrizations.R")
-source("Scripts/Sensitivity_analysis/sa1&2_parametrization.R")
+# source("Scripts/Sensitivity_analysis/sa1&2_parametrization.R")
 
 states<-names_stacked
 
 ## Loading Coef and Vcov, in case to not re-run the model all again
 ## Coef and Vcov SA1
-coef_sa1<-vroom("Outputs/Tables/Sensitivity_analysis/SA1_coefficients_gnm_for_all.csv.xz")
-vcov_sa1<-vroom("Outputs/Tables/Sensitivity_analysis/SA1_vcov_gnm_for_all.csv.xz")
+coef_sa1<-vroom("Outputs/Tables/Sensitivity_analysis/Newrun/SA1_coefficients_gnm_for_all.csv.xz")
+colnames(coef_sa1) <- c("abbrev_state", "b1", "b2", "b3", "b4")
+vcov_sa1<-vroom("Outputs/Tables/Sensitivity_analysis/Newrun/SA1_vcov_gnm_for_all.csv.xz")
 
 ## Coef and Vcov SA2
-coef_sa2<-vroom("Outputs/Tables/Sensitivity_analysis/SA2_coefficients_gnm_for_all.csv.xz")
-vcov_sa2<-vroom("Outputs/Tables/Sensitivity_analysis/SA2_vcov_gnm_for_all.csv.xz")
+coef_sa2<-vroom("Outputs/Tables/Sensitivity_analysis/Newrun/SA2_coefficients_gnm_for_all.csv.xz")
+vcov_sa2<-vroom("Outputs/Tables/Sensitivity_analysis/Newrun/SA2_vcov_gnm_for_all.csv.xz")
 
 # Putting coef and vcov in the right format again
 # coef 
@@ -63,6 +64,50 @@ summary(mv_sa2)
 
 ## Predictions from the meta-analysis
 # 3.1. Prediction overall without centering
+dengue_t2m_means<-dengue_t2m %>%
+  group_by(abbrev_state) %>%
+  summarise(tmin = min(temp_mean),
+            tmax = max(temp_mean)) %>%
+  ungroup() %>%
+  summarise(tmin = mean(tmin),
+            tmax = mean(tmax))
+
+## SA 1, 3 knots, equally distant
+knotsper_sa1<-equalknots(dengue_t2m_means$tmin:dengue_t2m_means$tmax, 
+                         nk = 3)
+varfun_sa1<-"ns"
+
+nlag_sa1<-21
+xlag_sa1<-0:nlag_sa1
+lagnk_sa1 <- 3
+klag_sa1<-logknots(nlag_sa1,lagnk_sa1)
+lagfun_sa1<-"ns"
+
+#
+argvar_sa1<-list(fun=varfun_sa1, 
+             Bound = range(dengue_t2m$temp_mean, na.rm = T), 
+             knots=knotsper_sa1, 
+             int=F)
+arglag_sa1<-list(fun=lagfun_sa1, 
+             knots=klag_sa1,
+             int=T)
+
+## Tpred
+tpred<-quantile(dengue_t2m$temp_mean, probs=(1:99)/100, na.rm=T)
+
+cb_sa1 <- crossbasis(tpred, 
+                     lag=nlag_sa1, 
+                     argvar=argvar_sa1, 
+                     arglag=arglag_sa1)
+bvar_sa1 <- do.call("onebasis",
+                    c(list(x=tpred),
+                      attr(cb_sa1,
+                           "argvar_sa1")))
+blag_sa1 <- do.call("onebasis",
+                    c(list(x=xlag_sa1),
+                      attr(cb_sa1,
+                           "arglag_sa1")))
+
 ## SA1
 Metapred_sa1<-crosspred(basis=bvar_sa1,
                     coef=coef(mv_sa1),
@@ -70,6 +115,43 @@ Metapred_sa1<-crosspred(basis=bvar_sa1,
                     at=tpred,
                     model.link="log")  
 plot(Metapred_sa1)
+
+## SA 2, 2 knots, equally distant, but fixed knots for the lag
+knotsper_sa2<-equalknots(dengue_t2m_means$tmin:dengue_t2m_means$tmax, 
+                         nk = 2)
+varfun_sa2<-"ns"
+
+nlag_sa2<-21
+xlag_sa2<-0:nlag_sa2
+lagnk_sa2 <- 4
+klag_sa2<-c(1,2,7,14)
+lagfun_sa2<-"ns"
+
+#
+argvar_sa2<-list(fun=varfun_sa2, 
+                 Bound = range(dengue_t2m$temp_mean, na.rm = T), 
+                 knots=knotsper_sa2, 
+                 int=F)
+arglag_sa2<-list(fun=lagfun_sa2, 
+                 knots=klag_sa2,
+                 int=T)
+
+## Tpred
+tpred<-quantile(dengue_t2m$temp_mean, probs=(1:99)/100, na.rm=T)
+
+cb_sa2 <- crossbasis(tpred, 
+                     lag=nlag_sa2, 
+                     argvar=argvar_sa2, 
+                     arglag=arglag_sa2)
+bvar_sa2 <- do.call("onebasis",
+                    c(list(x=tpred),
+                      attr(cb_sa2,
+                           "argvar_sa2")))
+blag_sa2 <- do.call("onebasis",
+                    c(list(x=xlag_sa2),
+                      attr(cb_sa2,
+                           "arglag_sa2")))
+
 ## SA2
 Metapred_sa2<-crosspred(basis=bvar_sa2,
                         coef=coef(mv_sa2),
